0f556c2488b2523ea0fc8c6c24d7be8fbe1f04d1
Amanda Maschio <amanda.maschio@metadados.com.br>
2024-07-14 18:59:18 -0300

    Added fastify/multipart

--- src/adapter/driver/server.ts
+++ src/adapter/driver/server.ts
@@ -1,4 +1,5 @@
 import fastifyCors from '@fastify/cors';
+import fastifyMultipart from '@fastify/multipart';
 import fastifySwagger from '@fastify/swagger';
 import fastifySwaggerUI from '@fastify/swagger-ui';
 import { routes } from '@routes/index';
@@ -10,40 +11,41 @@ import { jsonSchemaTransform } from 'fastify-type-provider-zod';
 export const app = fastify();
 
 app.register(fastifyCors, {
-	origin: '*',
+    origin: '*',
 });
 
 app.register(fastifySwagger, {
-	swagger: {
-		consumes: ['application/json'],
-		produces: ['application/json'],
-		info: {
-			title: 'FIAP - Tech Challenge',
-			description:
-				'Especificações da API para o back-end da aplicação de restaurante FIAP Tech Challenge.',
-			version: '1.0.0',
-		},
-	},
-	transform: jsonSchemaTransform,
+    swagger: {
+        consumes: ['application/json', 'multipart/form-data'],
+        produces: ['application/json', 'multipart/form-data'],
+        info: {
+            title: 'FIAP - Tech Challenge',
+            description: 'Especificações da API para o back-end da aplicação de restaurante FIAP Tech Challenge.',
+            version: '1.0.0',
+        },
+    },
+    transform: jsonSchemaTransform,
 });
 
 app.register(fastifySwaggerUI, {
-	routePrefix: '/docs',
+    routePrefix: '/docs',
 });
+
+app.register(fastifyMultipart);
 
 app.register(routes);
 
 app.setErrorHandler(errorHandler);
 
 async function run() {
-	await app.ready();
+    await app.ready();
 
-	await app.listen({
-		port: Number(process.env.API_PORT) || 3333,
-		host: '0.0.0.0',
-	});
+    await app.listen({
+        port: Number(process.env.API_PORT) || 3333,
+        host: '0.0.0.0',
+    });
 
-	logger.info('Documentation running at http://localhost:3333/docs');
+    logger.info('Documentation running at http://localhost:3333/docs');
 }
 
 run();
